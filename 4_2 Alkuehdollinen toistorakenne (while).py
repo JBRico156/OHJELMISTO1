b40417deee327a22639df1e28b3e047411dee16a
Jan B <janbarra@hotmail.com>
2025-01-31 09:43:49 +0200

    Update 4_2 Alkuehdollinen toistorakenne (while).py

--- 4_2 Alkuehdollinen toistorakenne (while).py
+++ 4_2 Alkuehdollinen toistorakenne (while).py
@@ -2,7 +2,9 @@
 #Sen jälkeen ohjelma lopettaa toimintansa. 1 tuuma = 2,54 cm
 cm = 2.54
 tuuma = 1
-while tuuma > 0:
+while tuuma >= 0:
     tuuma = int(input("Anna tuuma määrä(negatiivinen luku lopettaa ohjelman): "))
+    if tuuma < 0:
+        break
     print(f"{tuuma} tuumaa on {tuuma * cm} senttimetriä")
 print("Kiitos ja näkemiin!")
